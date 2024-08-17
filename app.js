84029c9954e5d4be3bbe00703d22c2b1940f035c
vavpyropark <vavpyropark24@gmail.com>
2024-08-17 21:46:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const addDataToHTML = () => {
             newProduct.innerHTML =  `
                 <img src="${product.image}" alt="">
                 <h2>${product.title}</h2>
-                <div class="price"><span>MRP. ${product.price} </span>Our Price.${Math.floor((product.price*0.3))}</div>
+                <div class="price"><span>MRP. ${product.price} </span>Rs.${Math.floor((product.price*0.3))}</div>
                  <button class="addCart" data-id="${product.id}">Add to Cart</button>
                 `;
                 listProductHTML.appendChild(newProduct);
